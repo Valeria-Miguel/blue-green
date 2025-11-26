9073787e740a3eb4a387e8fdecb350e614fc31b6
Dulce Valeria Miguel Juan <2022371092@uteq.edu.mx>
2025-11-26 16:48:38 -0600

    prueba

--- scripts/status.sh
+++ scripts/status.sh
@@ -6,12 +6,12 @@ echo "Contenedores activos:"
 docker ps --filter "name=app-" --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
 echo
 echo "Nginx - Upstream activo:"
-sudo grep "server 127.0.0.1" /etc/nginx/sites-available/blue-green.conf | head -1
+sudo grep "server 127.0.0.1" /etc/nginx/sites-available/app.conf | head -1
 echo
-echo "Health check Blue (8081):"
-curl -s http://127.0.0.1:8081 || echo "No responde"
-echo "Health check Green (8082):"
-curl -s http://127.0.0.1:8082 || echo "No responde"
+echo "Health check Blue (3001):"
+curl -s http://127.0.0.1:3001 || echo "No responde"
+echo "Health check Green (3002):"
+curl -s http://127.0.0.1:3002 || echo "No responde"
 echo
 echo "Servicio público:"
 curl -s http://127.0.0.1/health || echo "Inactivo"
