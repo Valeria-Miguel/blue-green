53650e4c1d400b79d9f190b8900001cd19dcf7e2
Dulce Valeria Miguel Juan <2022371092@uteq.edu.mx>
2025-11-26 12:02:11 -0600

    Deploy versión GREEN con fondo rojo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div style={{
       minHeight: "100vh",
-      background: "#264653",
+      background: "#e63946",
       padding: "3rem",
       color: "white",
       fontFamily: "Arial",
