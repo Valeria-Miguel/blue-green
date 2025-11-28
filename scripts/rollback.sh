61f933b7a1eb374e5cb529be76dc5b4aed55a947
Dulce Valeria Miguel Juan <2022371092@uteq.edu.mx>
2025-11-28 02:30:07 -0600

    morado

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -1,10 +1,14 @@
+
+
 #!/bin/bash
 
 CONF="/etc/nginx/sites-available/app.conf"
 
 # Regresar siempre a 3001
-sed -i 's/server 127\.0\.0\.1:300[1-2];/server 127.0.0.1:3001;/' $CONF
+sudo sed -i 's/server 127\.0\.0\.1:300[1-2];/server 127.0.0.1:3001;/' $CONF
 
-nginx -t && systemctl reload nginx
+# probar sintaxis
+sudo nginx -t
 
-echo "Rollback a Blue (3001)"
+# recargar nginx
+sudo systemctl reload nginx
