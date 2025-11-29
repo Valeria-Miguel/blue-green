08cf99b5cbeaca44ea3eea494bb52a494fe55daf
Dulce Valeria Miguel <2022371092@uteq.edu.mx>
2025-11-28 21:13:50 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     }}>
       
       <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>
-        Blue-Green Deployment
+        Blue-Green Deploymenttttt
       </h1>
       <h1 style={{ fontSize: "3.5rem" }}>
   GREEN DESPLEGADO AUTOMÁTICAMENTE
